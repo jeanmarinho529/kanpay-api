bb4ee30198cc46d36114cf92473a026a1e464854
Jean Marinho <jeanmarinho529@gmail.com>
2024-04-25 13:49:44 -0300

    feat: add batch file fake in seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Enums\BatchFileStatusEnum;
+use App\Models\BatchFile\BatchFile;
+use App\Models\BatchFile\BatchFileStatus;
+use App\Models\BatchFile\BatchFileType;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,5 +19,16 @@ class DatabaseSeeder extends Seeder
             BatchFileStatusSeeder::class,
             BatchFileTypeSeeder::class,
         ]);
+
+        BatchFile::factory()
+            ->hasBatchFileItemError(5)
+            ->create([
+                'name' => 'file_test-seed.csv',
+                'batch_file_type_id' => BatchFileType::first(),
+                'batch_file_status_id' => BatchFileStatus::where('name', BatchFileStatusEnum::PARTIAL->value)->first(),
+                'total_items' => 10,
+                'total_done' => 5,
+                'total_failed' => 5,
+            ]);
     }
 }
