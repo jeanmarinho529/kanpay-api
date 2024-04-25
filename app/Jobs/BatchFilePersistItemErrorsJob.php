6b6ad6ee68f2159a42368b20931104bcf3de3de9
Jean Marinho <jeanmarinho529@gmail.com>
2024-04-24 22:24:29 -0300

    feat: add file item validator

---
+++ app/Jobs/BatchFilePersistItemErrorsJob.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\BatchFile\BatchFileItemError;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class BatchFilePersistItemErrorsJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected array $data;
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public function handle(): void
+    {
+        BatchFileItemError::insert($this->data);
+    }
+}
